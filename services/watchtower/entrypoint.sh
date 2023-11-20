3789ab641a6713194b99a4425fba8471da7c3ede
Osherlevi7 <osher.levi7@gmail.com>
2023-11-20 15:01:07 +0200

    fix: move script

--- services/watchtower/entrypoint.sh
+++ services/watchtower/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-# Run GCR login for bootstrap time
+echo " Run GCR login for bootstrap time"
 /usr/local/bin/gcr_docker_login.sh
 
-# Run GCR login in cron for every 8 hours
+echo "Run GCR login in cron for every 8 hours"
 /usr/sbin/crond -b -l 8
 
-# Run watchtower as a non-root user
-exec su-exec appuser /watchtower "$@"
+echo "Run watchtower" 
+exec /watchtower "$@"
