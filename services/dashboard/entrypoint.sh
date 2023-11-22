5a4f38358476aba9d260ccd7e3295434ee6d1dff
Osherlevi7 <osher.levi7@gmail.com>
2023-11-22 18:20:36 +0200

    fix: add install to my entrypoint

--- services/dashboard/entrypoint.sh
+++ services/dashboard/entrypoint.sh
@@ -7,7 +7,7 @@ export FOO="Baz"
 if [ "${1}" == "serve" ]; then
     # This is the primary target
     shift # consume the 1st argument
-    # npm install --force && 
+    npm install --force 
     npm start
 else
     # An unknown command (debugging the container?): Forward as is
